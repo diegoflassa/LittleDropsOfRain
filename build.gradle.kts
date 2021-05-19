ee0e565a146dcb4d4e4f162462db90fff929c005
Diego Felipe Lassa <diegoflassa@gmail.com>
2021-05-19 18:27:45 -0300

    -Updated gradle-wrapper.properties to use the latest gradle -Updated some libraries in build.gradle -Removed some unused rules in proguard-rules.pro

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,23 +10,23 @@ buildscript {
     }
     dependencies {
         //classpath("com.android.tools.build:gradle:${Versions.gradle}")
-        classpath("com.android.tools.build:gradle:7.0.0-alpha15")
+        classpath("com.android.tools.build:gradle:7.1.0-alpha01")
         //classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlinCompilerVersion}")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.0")
         //classpath("com.google.gms:google-services:${Versions.google_services}")
-        classpath("com.google.gms:google-services:4.3.5")
+        classpath("com.google.gms:google-services:4.3.8")
         //classpath("androidx.navigation:navigation-safe-args-gradle-plugin:${Versions.safeargs_plugin}")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
 
         // Add the Crashlytics Gradle plugin.
         //classpath("com.google.firebase:firebase-crashlytics-gradle:${Versions.crashlytics}")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.5.2")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.6.1")
         // Performance Monitoring plugin
         //classpath("com.google.firebase:perf-plugin:${Versions.perf}")
-        classpath("com.google.firebase:perf-plugin:1.3.5")
+        classpath("com.google.firebase:perf-plugin:1.4.0")
         // Add the App Distribution Gradle plugin
         //classpath("com.google.firebase:firebase-appdistribution-gradle:${Versions.app_distribution}")
-        classpath("com.google.firebase:firebase-appdistribution-gradle:2.1.1")
+        classpath("com.google.firebase:firebase-appdistribution-gradle:2.1.2")
 
         //classpath("com.google.android.gms:oss-licenses-plugin:${Versions.oss_plugin}")
         classpath("com.google.android.gms:oss-licenses-plugin:0.10.4")
@@ -36,8 +36,8 @@ buildscript {
 }
 
 plugins {
-    id("com.diffplug.spotless") version "5.12.4" apply true
-    id("org.sonarqube") version "3.1.1" apply true
+    id("com.diffplug.spotless") version "5.12.5" apply true
+    id("org.sonarqube") version "3.2.0" apply true
 }
 
 subprojects {
