bb89a14883f8dce1d7f6464c13fdb78c2292d19d
Diego Felipe Lassa <diegoflassa@gmail.com>
2021-06-02 19:25:18 -0300

    -Added isShrinkResources to build.gradle.kts -Added some (commented) experimental flags to gradle.properties

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,6 +77,10 @@ android {
             // your project's release build type.
             isMinifyEnabled = true
 
+            // Enables resource shrinking, which is performed by the
+            // Android Gradle plugin.
+            isShrinkResources = true
+
             // Includes the default ProGuard rules files that are packaged with
             // the Android Gradle plugin. To learn more, go to the section about
             // R8 configuration files.
